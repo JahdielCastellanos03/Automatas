4db3a6ff009a630a713b84a9a5311fef6c5c3859
JahdielCastellanos03 <fadeamsc@gmail.com>
2024-11-13 22:02:09 -0500

    Delete PythonToJava directory

--- PythonToJava/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        System.out.println(suma(5,7));
-    }
-    public static int suma(int a, int b) {
-        return a+b;
-    }
-}
